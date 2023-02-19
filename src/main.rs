7321757266b15fa0bb91dc64633c6b62b2d236d9
Kai Geffen <geffenkai@gmail.com>
2023-02-18 22:32:32 -0500

    Implemented naive addition breaking into 2 words

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,11 @@ trait Reduce {
 }
 
 trait Add {
-    fn add(&self, x: Box<dyn Add>) -> u64;
+    fn add(&self, x: Self) -> u64;
 }
 
 trait Multiply {
-    fn multiply(&self, x: Box<dyn Add>) -> u64;
+    fn multiply(&self, x: Self) -> u64;
 }
 
 // Member of the modular arithmetic field
@@ -35,16 +35,43 @@ impl Reduce for Naive {
     }
 }
 impl Add for Naive {
-    fn add(&self, x: Box<dyn Add>) -> u64 {
-        return self.value;
+    fn add(&self, x: Self) -> u64 {
+        // We only need at most 2 words, since size will at most double
+        const N: usize = 2;
+
+        let mut carry: u64 = 0;
+        let mut ws: [u64; N] = [0; N + 1];
+        let mut us: [u64; N] = [0; N];
+        let mut vs: [u64; N] = [0; N];
+
+        us[0] = (&self.value << 32) >> 32;
+        vs[0] = (x.value << 32) >> 32;
+
+        us[1] = &self.value >> 32;
+        vs[1] = &self.value >> 32;
+
+        // Loop over the words
+        for i in 0..N {
+            ws[i] = (us[i] + vs[i] + carry).rem_euclid(self.modulo);
+            carry = (us[i] + vs[i] + carry) / self.modulo
+        }
+        ws[N] = carry;
+
+        return ws[0];
     }
 }
 impl Multiply for Naive {
-    fn multiply(&self, x: Box<dyn Add>) -> u64 {
+    fn multiply(&self, x: Self) -> u64 {
         return self.value;
     }
 }
 impl ModularArithmetic for Naive {}
+
+
+
+
+
+
 
 // Barret implementation of modular arithmetic
 struct Barret {
@@ -58,12 +85,12 @@ impl Reduce for Barret {
     }
 }
 impl Add for Barret {
-    fn add(&self, x: Box<dyn Add>) -> u64 {
+    fn add(&self, x: Self) -> u64 {
         return self.value;
     }
 }
 impl Multiply for Barret {
-    fn multiply(&self, x: Box<dyn Add>) -> u64 {
+    fn multiply(&self, x: Self) -> u64 {
         return self.value;
     }
 }
@@ -78,6 +105,8 @@ struct Montgomery {
 fn main() {
 
     println!("Hello, world!");
-    let foo = Naive{value: 133, modulo: 40};
-    println!("{}", foo.reduce());
+    let a = Naive{value: 3, modulo: 7};
+    let b = Naive{value: 4, modulo: 7};
+
+    println!("{}", a.add(b));
 }
