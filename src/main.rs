8cc3d763a89415a0614afb7f46fd4b2667e4a05c
Kai Geffen <geffenkai@gmail.com>
2023-02-18 21:37:11 -0500

    Stubbed modular arithmetic traits for naive and barrett

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#![allow(unused_variables)]
 
 // A number in modular arithmetic
 struct Number {
@@ -10,13 +11,16 @@ trait Reduce {
     fn reduce(&self) -> u64;
 }
 
-// trait Add {
-//     fn add(&self, );
-// }
+trait Add {
+    fn add(&self, x: Box<dyn Add>) -> u64;
+}
 
-// trait Multiply {
-//     fn multiply(&self);
-// }
+trait Multiply {
+    fn multiply(&self, x: Box<dyn Add>) -> u64;
+}
+
+// Member of the modular arithmetic field
+trait ModularArithmetic: Reduce + Add + Multiply {}
 
 
 // A naive implementation of modular arithmetic
@@ -30,6 +34,17 @@ impl Reduce for Naive {
         return &self.value - q * &self.modulo;
     }
 }
+impl Add for Naive {
+    fn add(&self, x: Box<dyn Add>) -> u64 {
+        return self.value;
+    }
+}
+impl Multiply for Naive {
+    fn multiply(&self, x: Box<dyn Add>) -> u64 {
+        return self.value;
+    }
+}
+impl ModularArithmetic for Naive {}
 
 // Barret implementation of modular arithmetic
 struct Barret {
@@ -38,9 +53,21 @@ struct Barret {
 }
 impl Reduce for Barret {
     fn reduce(&self) -> u64 {
+        let q: u64 = &self.value / &self.modulo;
+        return &self.value - q * &self.modulo;
+    }
+}
+impl Add for Barret {
+    fn add(&self, x: Box<dyn Add>) -> u64 {
         return self.value;
     }
 }
+impl Multiply for Barret {
+    fn multiply(&self, x: Box<dyn Add>) -> u64 {
+        return self.value;
+    }
+}
+impl ModularArithmetic for Barret {}
 
 // Montgomery implementation of modular arithmetic
 // [x] = (xR) mod N
