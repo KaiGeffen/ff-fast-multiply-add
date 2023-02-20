006123ef4044ab4fbdae4907c20ff84cf7ce5785
Kai Geffen <geffenkai@gmail.com>
2023-02-19 19:47:32 -0500

    Stubbed modular arithmetic contructer taking another argument: Reduction function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-// A number in modular arithmetic
-struct Number {
-    value: u64,
-    modulo: u64
-}
-
-trait Reduce {
-    fn reduce(&self) -> u64;
-}
-
 trait Add {
     fn add(&self, x: Self) -> u64;
 }
@@ -19,22 +9,15 @@ trait Multiply {
     fn multiply(&self, x: Self) -> u64;
 }
 
-// Member of the modular arithmetic field
-trait ModularArithmetic: Reduce + Add + Multiply {}
+type ReductionFunc = fn(u64, u64) -> u64;
 
-
-// A naive implementation of modular arithmetic
-struct Naive {
+#[derive(Default)]
+struct ModularArithmetic {
     value: u64,
-    modulo: u64
+    modulo: u64,
+    // reduce_f: ReductionFunc
 }
-impl Reduce for Naive {
-    fn reduce(&self) -> u64 {
-        let q: u64 = &self.value / &self.modulo;
-        return &self.value - q * &self.modulo;
-    }
-}
-impl Add for Naive {
+impl Add for ModularArithmetic {
     fn add(&self, x: Self) -> u64 {
         // We only need at most 2 words, since size will at most double
         const N: usize = 2;
@@ -44,6 +27,7 @@ impl Add for Naive {
         let mut us: [u64; N] = [0; N];
         let mut vs: [u64; N] = [0; N];
 
+        // TODO Add a utility method that generalizes this
         us[0] = (&self.value << 32) >> 32;
         vs[0] = (x.value << 32) >> 32;
 
@@ -61,73 +45,52 @@ impl Add for Naive {
         return ws[0] + (ws[1] << 32);// + ws[2] << 64;
     }
 }
-impl Multiply for Naive {
+impl Multiply for ModularArithmetic {
     fn multiply(&self, x: Self) -> u64 {
-        return self.value;
-    }
-}
-impl ModularArithmetic for Naive {}
-
-
-
-
-
-
-
-// Barret implementation of modular arithmetic
-struct Barret {
-    value: u64,
-    modulo: u64
-}
-impl Reduce for Barret {
-    fn reduce(&self) -> u64 {
         let q: u64 = &self.value / &self.modulo;
         return &self.value - q * &self.modulo;
     }
 }
-impl Add for Barret {
-    fn add(&self, x: Self) -> u64 {
-        return self.value;
-    }
-}
-impl Multiply for Barret {
-    fn multiply(&self, x: Self) -> u64 {
-        return self.value;
-    }
-}
-impl ModularArithmetic for Barret {}
 
-// Montgomery implementation of modular arithmetic
-// [x] = (xR) mod N
-struct Montgomery {
-
+// A naive implementation of modular arithmetic
+fn naive_reduce(value: u64, modulo: u64) -> u64 {
+    let q: u64 = value / modulo;
+    return value - q * modulo;
 }
+
+// All 3 of these structs add and multiply in the same way,
+// but after doing so they then reduce and either keep the reduced form
+// or convert to classical form.
+// The structure should instead be a modular arithmetic implementation
+// that also takes a reduce function!
+// The 3 cases to test are when a naive function is passed (Division)
+// the barret reduction, and the Montgomery reduction
 
 fn main() {
 
     println!("Hello, world!");
-    let a = Naive{value: 3, modulo: 1000};
-    let b = Naive{value: 4, modulo: 1000};
+    let a = ModularArithmetic{value: 3, modulo: 1000};
+    let b = ModularArithmetic{value: 4, modulo: 1000};
 
     println!("{}", a.add(b));
 }
 
 #[cfg(test)]
-mod naive {
+mod modular_arithmetic_works {
     use super::*;
 
     #[test]
     fn add_basic() {
-        let a = Naive{value: 3, modulo: 1000};
-        let b = Naive{value: 4, modulo: 1000};
+        let a = ModularArithmetic{value: 3, modulo: 1000};
+        let b = ModularArithmetic{value: 4, modulo: 1000};
 
         assert_eq!(a.add(b), 7);
     }
 
     #[test]
     fn add_large_numbers() {
-        let a = Naive{value: 27311837, modulo: u64::MAX};
-        let b = Naive{value: 88689789, modulo: u64::MAX};
+        let a = ModularArithmetic{value: 27311837, modulo: u64::MAX};
+        let b = ModularArithmetic{value: 88689789, modulo: u64::MAX};
 
         assert_eq!(a.add(b), 116001626);
     }
